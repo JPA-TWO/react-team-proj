6785072846349c7daf57270bd6bd6a1c1cac3bc4
ManaLi <yanhongli86@gmail.com>
2024-10-29 08:58:28 +0900

    change createStore to configureStore

--- src/Store.js
+++ src/Store.js
@@ -1,11 +1,11 @@
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from 'redux';
+import { configureStore } from 'redux';
 import { applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-const store = createStore(
+const store = configureStore(
 	rootReducer,
 	composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 );
