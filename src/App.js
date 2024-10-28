ce00eb7a6b1373d7ee2ac9ed10ed62866d357017
ManaLi <yanhongli86@gmail.com>
2024-10-28 12:33:24 +0900

    set up the folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Main from "./pages/Main";
+import Menus from "./pages/Menus";
+import MenuDetail from "./pages/MenuDetail";
+import MenuRegist from "./pages/MenuRegist";
+import MenuModify from "./pages/MenuModify";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import "./App.css";
+
+/* 추가 설치해야 하는 패키지 목록
+ * react-router-dom
+ * redux
+ * react-redux
+ * redux-actions
+ * redux-thunk
+ * redux-logger
+ * redux-devtools-extension
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Main />} />
+					<Route path="menu">
+						<Route index element={<Menus />} />
+						<Route path=":id" element={<MenuDetail />} />
+						<Route path="regist" element={<MenuRegist />} />
+						<Route path="modify">
+							<Route path=":id" element={<MenuModify />} />
+						</Route>
+					</Route>
+					<Route path="login" element={<Login />} />
+				</Route>
+				<Route path="*" element={<Error />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
