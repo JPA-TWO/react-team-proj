abdca4ffb87f76fd97d33ef5dbc9695c342c1dcb
Wondear <vytpals00@gmail.com>
2024-10-29 09:57:44 +0900

    [feature] insert book
    -책 추가 기능 완성

--- src/modules/MenuModule.js
+++ src/modules/MenuModule.js
@@ -4,20 +4,27 @@ const initalState = {};
 
 const GET_MENULIST = 'menu/GET_MENULIST';
 const GET_MENU = 'menu/GET_MENU';
+const REGIST_BOOK = 'menu/REGIST_BOOK';
 
-export const { menu: { getMenulist, getMenu } } = createActions({
+export const {
+	menu: { getMenulist, getMenu, registBook },
+} = createActions({
 	[GET_MENULIST]: (res) => ({ menulist: res }),
-	[GET_MENU]: (res) => ({ menu: res })
+	[GET_MENU]: (res) => ({ menu: res }),
+	[REGIST_BOOK]: (res) => ({ regist: res }),
 });
 
 const menuReducer = handleActions(
 	{
-		[GET_MENULIST]: (state, {payload}) => {
+		[GET_MENULIST]: (state, { payload }) => {
 			return payload;
 		},
-		[GET_MENU]: (state, {payload}) => {
+		[GET_MENU]: (state, { payload }) => {
 			return payload;
-		}
+		},
+		[REGIST_BOOK]: (state, { payload }) => {
+			return payload;
+		},
 	},
 	initalState
 );
